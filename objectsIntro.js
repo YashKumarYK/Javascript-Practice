b6125c7b26d1fb14a7bf50f6df66bb0dc8604e3b
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-07-11 14:53:11 +0530

    Added Object-Cloning Concept

--- objectsIntro.js
+++ objectsIntro.js
@@ -24,3 +24,56 @@ function Rectangle( len, bre){
 };
 let r2 = new Rectangle(4,6);
 console.log(r2);
+
+
+// OBJECT CLONING
+console.log("OBJECT CLONING");
+
+//lets make an object
+function Rectangle(len, bre){
+  this.length = len;
+  this.breadth= bre;
+  this.draw= function(){
+    console.log("Drawing");
+  }
+};
+
+let R1 = new Rectangle(2, 3);
+console.log(R1);
+
+let R2 = R1;
+R2.length = 1;
+console.log(R1);
+//The values got changed because we didn't copy the values of R1 to R2, instead of copying the values, 
+//we copied the address of R1, and then R2 points to the same address;
+
+
+let R = new Rectangle(4,5);
+// OBJECT CLONING #1 - ITERATION
+console.log("OBJECT CLONING #1 - Iteration");
+let R3 ={};
+for( let key in R){
+  R3[key] = R[key];
+}
+R3.length = 1;
+console.log(R3)
+console.log(R);
+// successfully cloned and the value of R didn't changed
+
+//OBJECT CLONING #2 - Assign
+console.log("OBJECT CLONING #2 - Assign");
+let R4 = Object.assign({},R);
+R4.breadth = 1;
+console.log(R4)
+console.log(R);
+//successfully cloned and the value of R didn't change
+
+//OBJECT CLONING #3 - Spread
+console.log("OBJECT CLONING #3 - Spread");
+let R5 = {...R};
+R5.breadth = 10;
+console.log(R5)
+console.log(R);
+
+
+
